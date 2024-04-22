5162b782509e65bd70af2dddb7e6323c23108580
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-04-22 17:36:58 +0200

    Removed wrongly named files

--- Directions.cpp
+++
@@ -1,9 +0,0 @@
-#include "Directions.h"
-
-int intFromDir(Directions dir){
-    return dir == Directions::REVERSE? 0 : 1;
-}
-
-Directions dirFromInt(int dir){
-    return dir == 0? Directions::REVERSE : Directions::FORWARD;
-};
