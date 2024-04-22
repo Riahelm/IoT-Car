8403e1167ca938f25385530e936f0bc9ceff4823
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-04-22 17:40:13 +0200

    Fixed Direction file

--- Direction.cpp
+++ Direction.cpp
@@ -1,9 +1,9 @@
-#include "Directions.h"
+#include "Direction.h"
 
-int intFromDir(Directions dir){
-    return dir == Directions::REVERSE? 0 : 1;
+int intFromDir(Direction dir){
+    return dir == Direction::REVERSE? 0 : 1;
 }
 
-Directions dirFromInt(int dir){
-    return dir == 0? Directions::REVERSE : Directions::FORWARD;
+Direction dirFromInt(int dir){
+    return dir == 0? Direction::REVERSE : Direction::FORWARD;
 };
