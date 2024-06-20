75a9af7d77e3244fc0e94d924c7377000c96287c
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-06-20 19:02:33 +0200

    Added missing pins

---
+++ BasicOperationPlus/src/Util/Pins.h
@@ -0,0 +1,23 @@
+#ifndef PINS_H
+#define PINS_H
+    
+    /* UTILIZED PINS */
+    #define LED_PIN 4
+
+    #define MOTORSTEP_SX_1 26
+    #define MOTORSTEP_SX_2 25
+    #define MOTORSTEP_SX_3 33
+    #define MOTORSTEP_SX_4 32
+
+    #define MOTORSTEP_DX_1 13
+    #define MOTORSTEP_DX_2 12
+    #define MOTORSTEP_DX_3 14
+    #define MOTORSTEP_DX_4 27
+ 
+    // HC-SR04 
+    #define DIST_TRIG 15
+    #define DIST_SX_ECHO 19
+    #define DIST_CE_ECHO 18
+    #define DIST_DX_ECHO 5
+
+#endif
