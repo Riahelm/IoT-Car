c8cfcc76aee80c0445cf3aa6bab5f885aa1cea09
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-07-11 17:24:08 +0200

    Added comments to semaphore module

--- BasicOperationPlus/src/MySemaphore/MySemaphore.h
+++ BasicOperationPlus/src/MySemaphore/MySemaphore.h
@@ -3,19 +3,22 @@
 #ifndef MYSEMAPHORE_H
 #define MYSEMAPHORE_H
 
-#include <mutex>
+/* Concurrency libraries */
+#include <mutex>                
 #include <condition_variable>
 
+/* The following class implements a generic semaphore, usable as both a binary and counting semaphore depending 
+   on how the release and acquire functions are used */
 class MySemaphore{
     public:
-        MySemaphore(int count);
-        ~MySemaphore();
-        void release();
-        void acquire();
+        MySemaphore(int count);     /* Semaphore constructor                                */
+        ~MySemaphore();             /* Semaphore destructor                                 */
+        void release();             /* Function to release permits                          */
+        void acquire();             /* Function to acquire permits, waits if count is == 0  */
     private:
-        std::mutex mtx;
-        std::condition_variable cv;
-        int count;
+        std::mutex mtx;             /* Sinchronization variable */
+        std::condition_variable cv; /* Sinchronization variable */
+        int count;                  /* Number of permits        */
 };
 
 #endif // MySemaphore.h
