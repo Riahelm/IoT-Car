80c798863640374c3950edb4aaadcaff7ee3a1e6
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-07-19 19:09:47 +0200

    Added comments

--- BasicOperationPlus/src/Util/StringParser/StringParser.h
+++ BasicOperationPlus/src/Util/StringParser/StringParser.h
@@ -4,6 +4,11 @@
 #include "Arduino.h"
 #include <tuple>
 
-std::tuple<String, String> splitFunc(String str);
+/* Function to split a function in two, delimited by a certain character.
+   Best used after String::trim() as it removes redundant characters, including '\r' and '\n' */
+std::tuple<String, String> splitFunc(String, char);
+
+/* Default function, delimits based on the whitespace character */
+std::tuple<String, String> splitFunc(String, char = ' ');
 
 #endif
