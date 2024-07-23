f887570f5eb89e350fe1b67f04f292bb8c913de8
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-07-23 19:48:55 +0200

    Started introducing simulated steppers, in SimulatedSteppers.cpp you can find a constructor initializer list

---
+++ BasicOperationPlus/src/Actuators/Steppers/Simulated/SimulatedSteppers.cpp
@@ -0,0 +1,20 @@
+#include "SimulatedSteppers.h"
+
+SimulatedSteppers::SimulatedSteppers(SimulationMap vmap) : map(vmap)
+{}
+
+void SimulatedSteppers::_goForwards (uint16_t millimeters){
+
+}         
+
+void SimulatedSteppers::_goBackwards(uint16_t millimeters){
+
+}         
+
+void SimulatedSteppers::_turnLeft   (uint16_t degrees){
+
+}             
+
+void SimulatedSteppers::_turnRight  (uint16_t degrees){
+
+} 
