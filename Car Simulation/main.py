6ecf444069db4a4c98b6d4866c25762f6f1ca63b
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-10-19 19:17:54 +0200

    Changed default safe distance

--- Car Simulation/main.py
+++ Car Simulation/main.py
@@ -5,9 +5,9 @@ from obstacle import Obstacle
 import numpy as np
 from matplotlib import pyplot as plt
 robot = Robot((50, 50), radius = 1, vision = 150, sensor_angle = 45, direction = 0, tolerance = 1)
-lat = Lattice(400, 600, robot, Goal(4, (300, 300)), safeDistance = 2 * robot.radius)
+lat = Lattice(400, 600, robot, Goal(4, (300, 300)), safeDistance = 0.5 + robot.radius)
 
 lat.randomize()
-
-lat.animate(skip = 10, max_its = 700, drop = 10)
+lat.plotPath(skip = 10, max_its = 700)
+#lat.animate(skip = 10, max_its = 700, drop = 10)
 
