b04605005c4a8f6e53dffa46c961538236f8baab
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-10-28 15:13:53 +0100

    Changed nomenclature

--- Car Simulation/main.py
+++ Car Simulation/main.py
@@ -1,15 +1,16 @@
-from lattice import Lattice
-from third_paper_lattice import Third_Paper_Lattice as TPL
-from third_paper_robot import Third_Paper_Robot as TPR
+from lattice import Third_Paper_Lattice as TPL
+from robot import ComplexRobot as CR 
+from robot import Third_Paper_Robot as TPR
 from goal import Goal
 from obstacle import Obstacle
 import numpy as np
 from matplotlib import pyplot as plt
 
-robot = TPR((50, 50), radius = 1, vision = 150, sensor_angle = 45, direction = 0, tolerance = 1)
-lat = TPL(400, 600, robot, Goal(50, (300, 300)), safe_distance = 0.5 + robot.radius)
-
-lat.randomize()
-#lat.plotPath(skip = 10, max_its = 700)
-lat.animate(skip = 10, max_its = 700, drop = 10)
+robot = CR((10, 10), radius = 1, vision = 0.5, sensor_angle = 90, direction = 0, sensor_tolerance = 15, tolerance = 1)
+lat = TPL(50, 50, robot, Goal(2, (40, 40)), safe_distance = robot.radius + 0.05)
+lat.addObstacle(Obstacle(1, (16,19)))
+#lat.genObs()
+#lat.randomize()
+lat.plotPath(skip = 10, max_its = 700)
+#lat.animate(skip = 1, max_its = 120, drop = 10)
 
