1eed6a9f5eb0b52585808b2eed4493075940427c
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-10-28 15:16:08 +0100

    Changed sensor angle to be in degrees in input, then converted to radians; Added ComplexRobot: has a sensor tolerance in degrees, then converted into radians. Added get_cone to create a Shapely (Library) polygon Added plot_cone to plot cone to screen

--- Car Simulation/robot.py
+++ Car Simulation/robot.py
@@ -1,5 +1,8 @@
+from typing import override
 import numpy as np
 import math
+from shapely.affinity import rotate, translate
+from shapely import Polygon
 from sphere import Sphere
 from matplotlib import pyplot as plt
 from util import bresenham
@@ -30,7 +33,7 @@ class Robot(Sphere):
         self.vision = float(kwargs.get('vision', self.radius * 6))
         self.tol = int(kwargs.get('tolerance', 2))
         self.direction = np.radians(float(kwargs.get('direction', 0)) % 360)
-        self.sensor_angle = float(kwargs.get('sensor_angle', 45))
+        self.sensor_angle = np.radians(float(kwargs.get('sensor_angle', 45)) % 360)
         self.path = [self.coords]
 
     def addMap(self, mapShape):
@@ -40,8 +43,8 @@ class Robot(Sphere):
 
     def seekObstacles(self, obstacleMap):
         found = False
-        leftDir  = self.direction + np.radians(self.sensor_angle)
-        rightDir = self.direction - np.radians(self.sensor_angle)
+        leftDir  = self.direction + self.sensor_angle
+        rightDir = self.direction - self.sensor_angle
 
         coords = []
         (foundT, coordsL) = self.mark_obstacles(obstacleMap, leftDir)
@@ -90,5 +93,79 @@ class Third_Paper_Robot (Robot):
     
 class ComplexRobot(Third_Paper_Robot):
     def __init__(self, coords, **kwargs):
+        """
+        Robot of spherical shape
+        
+        Optional parameters
+        -------------------
+        
+        radius : float
+                 radius of the robot (default 1)
+                 
+        vision : float
+                 sensor max distance of the robot (default 6 * radius)
+
+        tolerance: int
+                   radius of newly added obstacles for increased performance (default 2)
+        
+        direction: degrees
+                   initial direction of the robot (default 0)
+
+        sensor_angle: degrees
+                      fixed angular distance of sensors (default 45)
+        
+        sensor_tolerance: degrees
+                          fixed angular cone of vision (default sensor_angle)
+        """
         super().__init__(coords, **kwargs)
-        self.sensor_tolerance = kwargs.get('sensor_tolerance', self.sensor_angle)
+        self.sensor_tolerance = np.radians(float(kwargs.get('sensor_tolerance', self.sensor_angle)))
+
+    @override
+    def seekObstacles(self, obstacleMap):
+        found = False
+        leftDir  = self.direction + self.sensor_angle
+        rightDir = self.direction - self.sensor_angle
+
+        coords = []
+        self.mark_geometry(leftDir)
+        self.mark_geometry(self.direction)
+        self.mark_geometry(rightDir)
+        print("Crashing to avoid continuous plotting")
+        (foundT, coordsL) = super.mark_obstacles(obstacleMap, leftDir)
+        if foundT:
+            found = True
+        (foundT, coordsC) = super().mark_obstacles(obstacleMap, self.direction)
+        if foundT:
+            found = True
+        (foundT, coordsR) = super().mark_obstacles(obstacleMap, rightDir)
+        if foundT:
+            found = True
+        coords.append(coordsL)
+        coords.append(coordsC)
+        coords.append(coordsR)
+        return found, coords
+    
+
+    def get_cone(self, angle):
+        left_extreme = angle - self.sensor_tolerance
+        right_extreme = angle + self.sensor_tolerance
+
+        left_point = self.coords + self.vision * np.array([np.cos(left_extreme), np.sin(left_extreme)])
+        right_point = self.coords + self.vision * np.array([np.cos(right_extreme), np.sin(right_extreme)])
+
+        vertices = [tuple(self.coords), tuple(left_point), tuple(right_point)]
+        return Polygon(vertices)
+    
+    def plot_cone(self, cone):
+        x, y = cone.exterior.xy
+        x0, y0 = int(self.coords[0]), int(self.coords[1])
+
+        plt.plot(x0, y0, 'bo', markersize = 10, label = "Robot")
+        plt.fill(x, y, alpha = 0.3, fc='blue', label='Vision Cone')
+        plt.show()
+
+    def mark_geometry(self, angle):
+        
+        poly = self.get_cone(angle)
+        
+
