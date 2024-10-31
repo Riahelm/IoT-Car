809169198b13ab38a2c6c45e78903e743ec30534
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-10-31 12:35:27 +0100

    Committing other files as cleanup

--- Car Simulation/main.py
+++ Car Simulation/main.py
@@ -5,14 +5,15 @@ from obstacle import Obstacle
 import numpy as np
 from matplotlib import pyplot as plt
 
-robot = PR((10, 10), radius = 1, vision = 10, sensor_angle = 45, direction = 0, sensor_tolerance = 30, tolerance = 1)
-lat = PL(50, 50, robot, Goal(2, (40, 40)), safe_distance = robot.radius + 0.05)
-lat.addObstacle(Obstacle(1, (16,17)))
-lat.addObstacle(Obstacle(1, (30,20)))
-lat.addObstacle(Obstacle(1, (8,18)))
-#lat.addObstacle(Obstacle(1, (25,18)))
-#lat.genObs()
-#lat.randomize()
-#lat.plotPath(skip = 10, max_its = 700)
-lat.animate(skip = 1, max_its = 120, drop = 1)
+alpha = 1
+lat_size = 100
+vision = lat_size/5
+robot = PR((20, 20), radius = 1, vision = vision, sensor_angle = 30, direction = 0, sensor_tolerance = 15, scale = alpha)
+radius = (robot.vision * robot.scale) / 2
+goal = Goal(radius, (90, 90))
+lat = PL(lat_size, lat_size, robot, goal, safe_distance = robot.radius + 0.05)
+
+lat.animate(skip = 1, max_its = 100, drop = 1)
+
+
 
