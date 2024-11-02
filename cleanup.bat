86c6383c464a66da259accfb6dfcd0039ce2d07e
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-11-02 11:50:27 +0100

    Added recursive file search (might not be mandatory)

--- cleanup.bat
+++ cleanup.bat
@@ -4,7 +4,7 @@ set "folder_path=%cd%\Car Simulation/Tests/Results"
 echo Deleting all files in %folder_path%...
 
 rem Deletes all files in the specified local folder, but not subdirectories
-del /q "%folder_path%\*.png"
+del /q "%folder_path%\**.png"
 
 echo All files in %folder_path% have been deleted.
 pause  REM Pauses to show the result before closing the window
