a560cc6e7959f2e7143be473268a7ae0b0a9a09f
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-11-02 17:50:38 +0100

    Added a function to setup files

--- Car Simulation/main.py
+++ Car Simulation/main.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from lattice import Polygon_Lattice as PL
 from robot import Polygon_Robot as PR
 from control import Control_Lattice
@@ -5,11 +7,32 @@ from control import Control_Robot
 from goal import Goal
 from util import createConfig, loadConfig
 
+def setup_files(alphas):
+
+    files = [f"Car Simulation/Tests/Results/Data/Exp_{alpha}" for alpha in alphas]
+    for file in files:
+        try:
+            os.remove(file) # Delete file
+            os.remove(file + "_Full")
+        except FileNotFoundError:
+            pass    
+
+        with open(file, 'a') as f:
+            f.write(f"{'Result':<15}|{'Steps':<15}|{'Obstacles':<15}|Time\n")
+
+        with open(file + "_Full", 'a') as f:
+            f.write(f"{'Result':<15}|{'Steps':<15}|{'Obstacles':<15}|Time\n")
+
+def save_data(filename, data):
+    # Open the file in append mode
+    with open(filename, 'a') as file:
+        # Append a line of text
+        file.write(f"{data[0]:<15}|{data[1]:<15}|{data[2]:<15}|{data[3]}\n")
+        
 def gen_animations(alphas, lat_size, obstacle_count):
 
     vision = lat_size/5
     createConfig((lat_size, lat_size), obstacle_count, 2)
-    
     for alpha in alphas:
         radius = 1
         robot = PR((lat_size - 2 * radius, 1 + 2 * radius), radius = radius, vision = vision, sensor_tolerance = 15, scale = alpha, tolerance=1.2)
@@ -19,25 +42,38 @@ def gen_animations(alphas, lat_size, obstacle_count):
         lat.obstacleMap = loadConfig()
         lat.animate(i= alpha, skip = 1, max_its = 100, drop = 1)
 
-def gen_stats(alphas, lat_size, robot_radius, obstacle_count, obstacle_max_radius):
+def gen_stats(i, alphas, lat_size, robot_coords, robot_radius, goal_coords, obstacle_count, obstacle_max_radius):
 
     vision = lat_size/5
-    createConfig((lat_size, lat_size), obstacle_count, obstacle_max_radius)
-    
+    createConfig((lat_size, lat_size), robot_coords, goal_coords, obstacle_count, obstacle_max_radius)
+
     for alpha in alphas:
+        filename = f"Car Simulation/Tests/Results/Data/Exp_{alpha}"
+        
         radius = robot_radius
-        robot = Control_Robot((lat_size - 2 * radius, 1 + 2 * radius), radius = radius, vision = vision, sensor_tolerance = 15, scale = alpha, tolerance=1)
+        robot = Control_Robot(robot_coords, radius = radius, vision = vision, sensor_tolerance = 15, scale = alpha, tolerance=1)
         radius = lat_size/10
-        offset = lat_size/10
-        lat = Control_Lattice(lat_size, lat_size, robot, Goal(radius, (0 + offset,lat_size - offset)), safe_distance = 1 + 0.075)
+        lat = Control_Lattice(lat_size, lat_size, robot, Goal(radius, goal_coords), safe_distance = 1 + 0.075)
         lat.obstacleMap = loadConfig()
-        lat.plotPath(alpha, skip = 1, max_its = 200)
+        data = lat.plotPath(i, alpha, skip = 1, max_its = 200)
         lat.resetRobot()
-        lat.plotFullPath(alpha, skip = 1, max_its = 200) 
+        save_data(filename, data)
+        data = lat.plotPath(i, alpha, skip = 1, max_its = 200, full = True) 
+        save_data(filename + f"_Full", data)
+
 
 alphas = [1, 0.5, 0.25, 0.1]
 lat_size = 100
 robot_radius = 2
 obstacle_max_radius = 2
-obstacle_count = 20
-gen_stats(alphas, lat_size, robot_radius, obstacle_count, obstacle_max_radius)
+obstacle_count = 10
+exp_count = 20
+robot_coords = (lat_size - 2 * robot_radius, 1 + 2 * robot_radius)
+goal_coords = (0 + lat_size/10,lat_size - lat_size/10)
+
+subprocess.run(["cleanup.bat"], shell= False)
+
+setup_files(alphas)
+for i in range(exp_count):
+    gen_stats(i, alphas, lat_size, robot_coords, robot_radius, goal_coords, obstacle_count, obstacle_max_radius)
+    
